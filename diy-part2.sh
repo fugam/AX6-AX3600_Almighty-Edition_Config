91c4d1c834d30546bc52d41c982cd3a58b37ac4e
fugam <whojam@sina.cn>
2022-01-05 13:09:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,12 +27,6 @@ if [ $delete_bootstrap ]; then
   sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 fi
 
-#删除无用软件包pptp、zerotier、kms
-sed -i '/CONFIG_PACKAGE_luci-app-pptp-server=y/d' .config
-sed -i '/CONFIG_PACKAGE_luci-app-zerotier=y/d' .config
-sed -i '/CONFIG_PACKAGE_luci-app-vlmcsd=y/d' .config
-
-
 # 修改默认Lan IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
